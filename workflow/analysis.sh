91666bd6592b422f8009160d8baf55aa17be353f
Mr-Qasim-Hussain <qasimhussain470@yahoo.com>
2026-08-06 23:06:46 +0800

    update_analysis.sh

--- workflow/analysis.sh
+++ workflow/analysis.sh
@@ -1,1 +1,8 @@
 #!/usr/bin/env bash
+# Use bactopia to process a single sample from SRA/ENA
+bactopia \
+    --accession SRX4563634 \
+    --coverage 100 \
+    --genome_size 2800000 \
+    --max_cpus 2 \
+    --outdir ena-single-sample
